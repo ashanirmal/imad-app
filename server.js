69e62f018a3c1f1d593930d7dd1ba91ee695051b
ashanirmal <30681163+ashanirmal@users.noreply.github.com>
2017-08-10 11:21:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,7 +70,12 @@ app.get('/counter', function (req, res) {
     counter = counter + 1;
   res.send(counter.toString());
 });
-
+var names = [];
+app.get('/submit-name/:name', function (req, res) {
+    names.push(req.params.name);
+    //Send JSON
+  res.send(JSON>Stringify(names));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
