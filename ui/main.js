cb422454f005fe2dbb2a5bc97e298c6e82c2e9ac
ashanirmal <30681163+ashanirmal@users.noreply.github.com>
2017-08-10 11:00:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,14 +16,13 @@ var button=document.getElementById('counter');
 button.onclick = function() {
     var req = new XMLHttpRequest();
     req.onreadystatechange = function () {
-        console.log('request state : ' + req.readyState);
-        console.log('DONE : '+ XMLHttpRequest.DONE);
       if (req.readyState === XMLHttpRequest.DONE){
           console.log('request status : ' + req.status);
           if (req.status === 200){
+              console.log('response : ' + req.response.Text);
               var counter = req.response.Text;
               var span = document.getElementById('count');
-              span.innerHTML = counter.toString() + ' ';
+              span.innerHTML = counter + ' ';
           }
       }  
     };
