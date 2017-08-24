9be68b308c7644b1603fb17b9fcdf92902672aee
ashanirmal <30681163+ashanirmal@users.noreply.github.com>
2017-08-24 09:40:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/ui/madi.png', function (req, res) {
 function hash (input, salt)
 {
     var result = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return result;
+    return result.toString('hex');
 }
 app.get('/hash/:input', function (req, res) {
     var hashedstring = hash(req.params.input,'this-is-a-random-string');
