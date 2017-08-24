875aaff8994de11380d6962fd4657d5194d84ab3
ashanirmal <30681163+ashanirmal@users.noreply.github.com>
2017-08-24 22:46:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
-    secret: '',
+    secret: 'Some-Random-String',
     cookie: {maxAge: 1000 * 60 * 5} //5 min
 }));
 
