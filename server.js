cf401ddd44ea6ff65b54f91a7eded9999c2e3042
ashanirmal <30681163+ashanirmal@users.noreply.github.com>
2017-09-10 11:48:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,8 @@ app.post('/create-user', function (req, res) {
         if (err){
             res.status(500).send(err.toString());
         } else {
-            res.send("User successfully created : "+username);
+            responsejson["message"] = "User successfully created : "+username;
+            res.send(JSON.stringify(responsejson));
         }
     } );
 });
